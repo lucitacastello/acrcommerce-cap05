d01eac0d8af063bfebb72442b0402e38e8fe3c04
cidarosa <lucita.rosa@gmail.com>
2023-07-10 10:02:03 -0300

    BCrypt password encoder

--- src/main/java/com/acrdev/acrcommerce/AcrcommerceApplication.java
+++ src/main/java/com/acrdev/acrcommerce/AcrcommerceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AcrcommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AcrcommerceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AcrcommerceApplication.class, args);
+    }
 }
