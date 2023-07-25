b385c5e91b91066dbdff8411e79585f8678bc680
cidarosa <lucita.rosa@gmail.com>
2023-07-25 15:01:04 -0300

    Access control by role, by route

--- src/main/java/com/acrdev/acrcommerce/services/UserService.java
+++ src/main/java/com/acrdev/acrcommerce/services/UserService.java
@@ -17,6 +17,7 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
